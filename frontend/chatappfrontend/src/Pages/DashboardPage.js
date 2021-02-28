b7e658c80a44196721f763b0a29121bad21b0798
Archit <archit.hadge@gmail.com>
2021-02-28 14:46:46 +0530

    mapping done

--- frontend/chatappfrontend/src/Pages/DashboardPage.js
+++ frontend/chatappfrontend/src/Pages/DashboardPage.js
@@ -4,8 +4,12 @@ import { Link, withRouter } from 'react-router-dom';
 //comment for push
 const DashboardPage = (props) => {
     const [chatrooms, setChatrooms] = React.useState([]);
+    const [reforms, setReforms] = React.useState([]);
     const inputRef = React.useRef();
     const logoutRef = React.useRef();
+    const uploaderRef = React.useRef();
+    const textRef = React.useRef();
+    const departmentRef = React.useRef();
 
     const getChatrooms = () => {
         axios.get('http://localhost:8000/chatroom', {
@@ -23,14 +27,38 @@ const DashboardPage = (props) => {
         })
     }
 
+    const getReforms = () => {
+        axios.get('http://localhost:8000/reform', {
+            headers: {
+                Authorization: "Bearer " + localStorage.getItem("Token")
+            }
+        }).then((response) => {
+            console.log(response.data);
+            setReforms(response.data);
+        }).catch((err) => {
+            console.log(err);
+        })
+    }
+
     const createChatroom = () => {
         console.log('Creating chatroom')
         axios.post('http://localhost:8000/chatroom',
             { name: inputRef.current.value },
-            { headers: { Authorization: "Bearer " + localStorage.getItem("Token") } }).then(response=>{
+            { headers: { Authorization: "Bearer " + localStorage.getItem("Token") } }).then(response => {
                 // props.history.push('/dashboard');
                 window.location.reload();
             })
+    }
+
+    const createReform = () => {
+        axios.post('http://localhost:8000/reform', {
+            uploader: uploaderRef.current.value,
+            text: textRef.current.value,
+            department: departmentRef.current.value
+        }, { headers: { Authorization: "Bearer " + localStorage.getItem("Token") } }).then((response)=>{
+            console.log('success',response.data)
+        })
+        window.location.reload();
     }
 
     const logout = () => {
@@ -40,14 +68,21 @@ const DashboardPage = (props) => {
 
     React.useEffect(() => {
         getChatrooms();
+        getReforms();
     }, [])
     return (
         <div>
             <input type="text" name="chatroomName" id="chatroomName" ref={inputRef} />
             <button onClick={createChatroom}>Create chatroom</button>
-            {chatrooms.map(chatroom => (
-                <div key={chatroom._id} >Name is {chatroom.name}
-                    <Link to={"/chatroom/" + chatroom._id}>Join</Link>
+            <h1>Create reform</h1>
+            <input type='text' ref={uploaderRef} placeholder='uploader'></input>
+            <input type='text' ref={textRef}></input>
+            <input type='text' ref={departmentRef}></input>
+            <button onClick={createReform}>Post Reform</button>
+            {/* <button onClick={getReforms}>Getreform</button> */}
+            {reforms.map(reform => (
+                <div key={reform._id} >{reform.text}
+                    {/* <Link to={"/chatroom/" + chatroom._id}>Join</Link> */}
                 </div>
             ))}
             <button ref={logoutRef} onClick={logout}>Logout</button>
